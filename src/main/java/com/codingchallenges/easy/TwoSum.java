692e08c281a6465916d5fb24e838246531d6eafc
Olaniyi F <niyifalomo@gmail.com>
2020-05-17 00:07:58 -0700

    Code clean up

--- src/main/java/com/codingchallenges/easy/TwoSum.java
+++ src/main/java/com/codingchallenges/easy/TwoSum.java
@@ -3,14 +3,23 @@ import java.util.HashMap;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-//https://leetcode.com/problems/two-sum/
+/**
+ * https://leetcode.com/problems/two-sum/
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
 
 public class TwoSum {
     private static final Logger LOGGER = LogManager.getLogger(TwoSum.class.getName());
     public static void main(String[] args) {
         int[] nums = {3, 4, 5, 6, 7}; int target = 8;
         int[] indices = twoSum(nums, target);
-        LOGGER.debug(String.format("Indices: %s,%s", indices[0], indices[1]));
+        LOGGER.debug("Indices: {},{}", indices[0], indices[1]);
     }
 
     public static int[] twoSum(int[] nums, int target) {
